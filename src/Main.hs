5de361631c3314ef498d679c545fc7c5760b5701
Simon Shine <shreddedglory@gmail.com>
2013-08-12 22:08:46 +0200

    Pretty-printer printing pretty pretty

--- src/Main.hs
+++ src/Main.hs
@@ -6,10 +6,11 @@ import Control.Monad
 import Syntax
 import Lexer
 import Parser
+import Pretty
 
 -- cat ../data/foo.rf | ./Main
 main :: IO ()
 main = do
   prog <- getContents
   ast  <- return $ parse . tokenize $ prog
-  putStrLn (show ast)
+  putStrLn (pretty ast)
--- src/Pretty.hs
+++ src/Pretty.hs
@@ -8,8 +8,11 @@ import Syntax
 commas :: [String] -> String
 commas = intercalate ", "
 
-spaces :: Int -> String
-spaces n = replicate n ' '
+spaces :: [String] -> String
+spaces = intercalate " "
+
+indent :: Int -> String
+indent n = replicate n ' '
 
 pretty :: Prog -> String
 pretty = prettyProg
@@ -20,18 +23,35 @@ prettyProg = intercalate "\n\n" . map prettyDefn . progDefns
 prettyDefn :: Defn -> String
 prettyDefn (Defn (_, name) terms body) =
   "def " ++ name ++ " " ++
-  commas (map prettyId terms) ++ " :=\n" ++
-  prettyExp 4 body
+  spaces (map prettyId terms) ++ " :=\n" ++
+  indent 4 ++ prettyExp 4 body
+
+prettyId :: Id -> String
+prettyId = snd
 
 prettyExp :: Int -> Exp -> String
 prettyExp n exp =
-  spaces n ++
   case exp of
-    LeftExp le -> prettyLeftExp le
-    Let lhs fID termIDs body _ ->
-      "let " ++ snd lhs ++ " = " ++ snd fID ++ " " ++
-      intercalate " " (map snd termIDs) ++ " in " ++ prettyExp body
-    Case caseId cases _ ->
-      "case " ++ snd caseId ++ " of\n" ++
-      spaces n
+    LeftExp le -> prettyLeftExp n le
+    Let var fID termIDs body _ ->
+      let firstLetLine = "let " ++ snd var ++ " = " ++ snd fID ++ " " ++ spaces (map prettyId termIDs) ++ " in"
+          separator = "\n" ++ indent (n + length firstLetLine)
+          secondLetLine = prettyExp n body
+      in firstLetLine ++ separator ++ secondLetLine
+    Case caseId (c:cs) _ ->
+      "case " ++ prettyId caseId ++ " of\n" ++
+      indent (n+4) ++ prettyCase (n+2) c ++
+      concatMap (prefixCase . prettyCase n) cs
+  where
+    prefixCase s = indent n ++ "  | " ++ s
 
+prettyLeftExp :: Int -> LeftExp -> String
+prettyLeftExp n lexp =
+  case lexp of
+    Var (_, vname) -> vname
+    Nil _ -> "Nil"
+    Cons lexps _ -> "Cons (" ++ commas (map (prettyLeftExp n) lexps) ++ ")"
+
+prettyCase :: Int -> (LeftExp, Exp) -> String
+prettyCase n (lexp, exp) =
+  prettyLeftExp n lexp ++ " -> " ++ prettyExp n exp ++ "\n"
