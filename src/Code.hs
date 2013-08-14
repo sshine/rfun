30f333f6ca607483cd719445f9de282da4a9fbe6
Simon Shine <shreddedglory@gmail.com>
2013-08-14 21:53:45 +0200

    Modified code generator, improved PISA pretty-printer

--- src/Code.hs
+++ src/Code.hs
@@ -8,14 +8,55 @@ import Control.Applicative
 import Syntax
 import PISA
 
--- Constants
-nilC :: Int
-nilC = 0
+-- | The CodeGenEnv keeps track of register allocation, ...
+data CodeGenEnv = CodeGenEnv { regAllocStack :: [Reg]
+                             , regAllocNext  :: Reg }
+              deriving (Show, Eq)
 
-consC :: Int
-consC = 1
 
--- A collection of special-purpose registers:
+-- | The CodeGen monad
+type CodeGen a = State CodeGenEnv a
+
+
+-- | Translate a given program, remove monad after use
+translate :: Prog -> [PISA]
+translate prog =
+  fst $ runState (translateProg prog) initState
+  where initState = CodeGenEnv { regAllocStack = []
+                               , regAllocNext = regAllocStart }
+
+
+-- | Translate a given program within a CodeGen monad
+translateProg :: Prog -> CodeGen [PISA]
+translateProg = liftM concat . mapM translateDefn . progDefns
+
+translateDefn :: Defn -> CodeGen [PISA]
+translateDefn defn = return [] -- TODO
+
+
+-- | Translate left-expressions (Nil, Cons, Var)
+translateLeftExp :: LeftExp -> Reg -> CodeGen [PISA]
+translateLeftExp (Nil _) regDest =
+  return [ BRA "getfree"
+         , XORI regDest nilC
+         , EXCH regDest regCell ]
+
+translateLeftExp (Cons (l:r:_) _) regDest = do
+      regL <- allocReg
+      regR <- allocReg
+      leftTreeCode <- translateLeftExp l regL
+      rightTreeCode <- translateLeftExp r regR
+      return $ leftTreeCode ++ rightTreeCode ++
+        [ BRA "getfree"        -- regCell <- get_free()
+        , XORI regDest consC   -- regDest <- cons
+        , ADDI regCell 1       -- regCell <- regCell + 1
+        , EXCH regL regCell    -- regL <-> M(regCell)
+        , ADDI regCell 1       -- regCell <- regCell + 1
+        , EXCH regR regCell    -- regR <-> M(regCell)
+        , SUBI regCell 2       -- regCell <- regCell - 2
+        ]
+
+---------- A collection of special-purpose registers: ----------
 
 -- Zero register
 regZero :: Reg
@@ -43,20 +84,34 @@ regCell :: Reg
 regCell = 5
 
 -- Minimal free register
-regAlloc :: Reg
-regAlloc = 6
+regAllocStart :: Reg
+regAllocStart = 6
 
--- Variable counting
-type CodeGen a = State Int a
+-- Assume that an arbitrary amount of symbolic registers exist, but allow for
+-- returning used ones to a pool.  When it can be asserted, during compilation,
+-- that a register contains zero and will not be needed locally, return it to
+-- the allocator.  Do not regard spilling yet.  This is very manual.
 
-runCodeGen :: CodeGen a -> (a, Int)
-runCodeGen m = runState m regAlloc
+allocReg :: CodeGen Reg
+allocReg = do
+  env <- get
+  case regAllocStack env of
+    [] -> do
+      put $ env { regAllocNext = regAllocNext env + 1 }
+      return $ regAllocNext env
+    reg:stack -> do
+      put $ env { regAllocStack = stack }
+      return reg
 
-newReg :: CodeGen Reg
-newReg = do
-  reg <- get
-  put (reg+1)
-  return reg
+freeReg :: Reg -> CodeGen ()
+freeReg reg = modify $ \env -> env { regAllocStack = reg:regAllocStack env }
+
+-- Constants
+nilC :: Int
+nilC = 0
+
+consC :: Int
+consC = 1
 
 -- wrap a function body in call-convention code (from article)
 callwrapper :: Label -> [PISA] -> [PISA]
@@ -64,7 +119,7 @@ callwrapper flab fcode =
   [ LABEL ftop, BRA  fbot
               , SUBI regSp 1
               , EXCH regRetOff regSp
-  , LABEL flab, SWAPBR regRetOff
+  , LABEL fmid, SWAPBR regRetOff
               , NEG  regRetOff
               , EXCH regRetOff regSp
               , ADDI regSp 1 ]
@@ -72,13 +127,14 @@ callwrapper flab fcode =
   [ LABEL fbot, BRA ftop ]
   where
     ftop = flab ++ "_top"
+    fmid = flab ++ "_mid"
     fbot = flab ++ "_bot"
 
 -- getfree code, to be placed once in the generated code.
 -- combination of article 1 (get_free pseudocode) and clean p. 155
-getFree :: Reg -> [PISA]
-getFree destReg =
-  callwrapper undefined   --  wrap getFree
+getFreeCode :: Reg -> [PISA]
+getFreeCode destReg =
+  callwrapper "getfree"
   [ LABEL "getfree"     , BNE  regFlp regZero "getfree_else"
                         , XOR  destReg regHp
                         , ADDI regHp 3
@@ -88,25 +144,3 @@ getFree destReg =
                         , SWAP destReg regFlp   --   M(regFlp)
   , LABEL "getfree_end" , BEQ  regFlp regZero "TODO..." ]
 
-
--- Translate Nil, Cons constants
-translateLeftExp :: LeftExp -> Reg -> CodeGen [PISA]
-translateLeftExp (Nil _) regDest =
-  return [ BRA "getfree"
-         , XORI regDest nilC
-         , EXCH regDest regCell ]
-
-translateLeftExp (Cons (l:r:_) _) regDest = do
-      regL <- newReg
-      regR <- newReg
-      leftTreeCode <- translateLeftExp l regL
-      rightTreeCode <- translateLeftExp r regR
-      return $ leftTreeCode ++ rightTreeCode ++
-        [ BRA "getfree"        -- regCell <- get_free()
-        , XORI regDest consC   -- regDest <- cons
-        , ADDI regCell 1       -- regCell <- regCell + 1
-        , EXCH regL regCell    -- regL <-> M(regCell)
-        , ADDI regCell 1       -- regCell <- regCell + 1
-        , EXCH regR regCell    -- regR <-> M(regCell)
-        , SUBI regCell 2       -- regCell <- regCell - 2
-        ]
--- src/PISA.hs
+++ src/PISA.hs
@@ -1,6 +1,10 @@
 
 module PISA where
 
+import Data.List
+
+import Pretty -- spaces
+
 type Reg = Int
 type Imm = Int
 type Mem = Int
@@ -32,3 +36,53 @@ data PISA = -- Data instructions
   | SWAP Reg Reg  -- not in PISA article, but in heap article
   | LABEL Label
   deriving (Show, Eq)
+
+prettyPISA :: [PISA] -> String
+prettyPISA prog = go prog
+  where
+    go (LABEL lab : instr : prog') = lab ++ ": " ++
+                                     indent (longestLabel - length lab) ++
+                                     show instr ++ "\n" ++
+                                     go prog'
+    go (instr : prog') = indent (longestLabel+2) ++ show instr ++ "\n" ++ go prog'
+    go [] = []
+    
+    longestLabel :: Int
+    longestLabel = foldr (\label len -> labelLength label `max` len) 0 prog
+
+labelLength :: PISA -> Int
+labelLength (LABEL label) = length label 
+labelLength _ = 0
+
+prettyInstr :: PISA -> String
+prettyInstr instr =
+  case instr of
+    ADD r1 r2     -> "ADD "  ++ prettyRegs [r1, r2]
+    SUB r1 r2     -> "SUB "  ++ prettyRegs [r1, r2]
+    NEG r         -> "NEG "  ++ prettyReg r
+    XOR r1 r2     -> "XOR "  ++ prettyRegs [r1, r2]
+    ADDI r im     -> "ADDI " ++ prettyReg r ++ prettyImm im
+    SUBI r im     -> "SUBI " ++ prettyReg r ++ prettyImm im
+    XORI r im     -> "XORI " ++ prettyReg r ++ prettyImm im
+    ORX r1 r2 r3  -> "ORX "  ++ prettyRegs [r1, r2, r3]
+    ANDX r1 r2 r3 -> "ANDX " ++ prettyRegs [r1, r2, r3]
+    SLTX r1 r2 r3 -> "SLTX " ++ prettyRegs [r2, r2, r3]
+    EXCH r1 r2    -> "EXCH " ++ prettyRegs [r1, r2]
+    BRA lab       -> "BRA "  ++ lab
+    RBRA lab      -> "RBRA " ++ lab
+    BEQ r1 r2 lab -> "BEQ "  ++ prettyRegs [r1, r2] ++ " " ++ lab
+
+prettyRegs :: [Reg] -> String
+prettyRegs = intercalate " " . map prettyReg
+
+prettyReg :: Reg -> String
+prettyReg 0 = "$0"
+prettyReg 1 = "$hp"
+prettyReg 2 = "$sp"
+prettyReg 3 = "$ro"
+prettyReg 4 = "$flp"
+prettyReg 5 = "$cell"
+prettyReg n = show n
+
+prettyImm :: Imm -> String
+prettyImm = show
